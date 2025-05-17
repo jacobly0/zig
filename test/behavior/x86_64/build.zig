bafa7ad6899a509b83a2b037c70c03000a9fa23e
Jacob Young <jacobly0@users.noreply.github.com>
2025-05-17 17:57:14 -0400

    working

--- test/behavior/x86_64/build.zig
+++ test/behavior/x86_64/build.zig
@@ -5,121 +5,108 @@ pub fn build(b: *std.Build) void {
         "test-filter",
         "Skip tests that do not match any filter",
     ) orelse &[0][]const u8{};
-
-    const compiler_rt_lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "compiler_rt",
-        .use_llvm = false,
-        .use_lld = false,
-        .root_module = b.createModule(.{
-            .root_source_file = b.addWriteFiles().add("compiler_rt.zig", ""),
-            .target = b.resolveTargetQuery(.{ .cpu_arch = .x86_64 }),
-        }),
-    });
-    compiler_rt_lib.bundle_compiler_rt = true;
-
     for ([_]std.Target.Query{
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{.bsf_bsr_0_clobbers_result}),
-            //.cpu_features_sub = std.Target.x86.featureSet(&.{.sse}),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{.bsf_bsr_0_clobbers_result}),
-            .cpu_features_sub = std.Target.x86.featureSet(&.{
-                .cmov,
-                //.sse,
-                .sse2,
-            }),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{.sahf}),
-            .cpu_features_sub = std.Target.x86.featureSet(&.{.cmov}),
-        },
         //.{
         //    .cpu_arch = .x86_64,
         //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
-        //    .cpu_features_sub = std.Target.x86.featureSet(&.{.sse}),
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{.bsf_bsr_0_clobbers_result}),
+        //    //.cpu_features_sub = std.Target.x86.featureSet(&.{.sse}),
         //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{.bsf_bsr_0_clobbers_result}),
+        //    .cpu_features_sub = std.Target.x86.featureSet(&.{
+        //        .cmov,
+        //        //.sse,
+        //        .sse2,
+        //    }),
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{.sahf}),
+        //    .cpu_features_sub = std.Target.x86.featureSet(&.{.cmov}),
+        //},
+        ////.{
+        ////    .cpu_arch = .x86_64,
+        ////    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
+        ////    .cpu_features_sub = std.Target.x86.featureSet(&.{.sse}),
+        ////},
         .{
             .cpu_arch = .x86_64,
             .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
             .cpu_features_sub = std.Target.x86.featureSet(&.{.sse2}),
         },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{ .adx, .gfni }),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{.sse3}),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{.ssse3}),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{.sse4_1}),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{.sse4_2}),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v2 },
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v2 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{ .adx, .gfni }),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v2 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{.avx}),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v3 },
-            .cpu_features_sub = std.Target.x86.featureSet(&.{.avx2}),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v3 },
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v3 },
-            .cpu_features_add = std.Target.x86.featureSet(&.{ .adx, .gfni }),
-        },
-        .{
-            .cpu_arch = .x86_64,
-            .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v4 },
-        },
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{ .adx, .gfni }),
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{.sse3}),
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{.ssse3}),
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{.sse4_1}),
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64 },
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{.sse4_2}),
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v2 },
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v2 },
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{ .adx, .gfni }),
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v2 },
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{.avx}),
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v3 },
+        //    .cpu_features_sub = std.Target.x86.featureSet(&.{.avx2}),
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v3 },
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v3 },
+        //    .cpu_features_add = std.Target.x86.featureSet(&.{ .adx, .gfni }),
+        //},
+        //.{
+        //    .cpu_arch = .x86_64,
+        //    .cpu_model = .{ .explicit = &std.Target.x86.cpu.x86_64_v4 },
+        //},
     }) |query| {
         const target = b.resolveTargetQuery(query);
         const cpu = query.serializeCpuAlloc(b.allocator) catch @panic("OOM");
         for ([_][]const u8{
-            "binary.zig",
+            //"binary.zig",
             "cast.zig",
             "mem.zig",
-            "unary.zig",
+            //"unary.zig",
         }) |path| {
             const test_mod = b.createModule(.{
                 .root_source_file = b.path(path),
@@ -132,10 +119,6 @@ pub fn build(b: *std.Build) void {
                 .use_lld = false,
                 .root_module = test_mod,
             });
-            if (!std.Target.x86.featureSetHas(target.result.cpu.features, .sse2)) {
-                test_exe.bundle_compiler_rt = false;
-                test_mod.linkLibrary(compiler_rt_lib);
-            }
             const test_run = b.addRunArtifact(test_exe);
             b.default_step.dependOn(&test_run.step);
             for ([_]*std.Build.Step{
